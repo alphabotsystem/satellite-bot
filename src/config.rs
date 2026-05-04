7a179d006e7da407b38ecfd27d0c845e21c9f49c
Matic Conradi <matic.conradi@me.com>
2026-05-04 21:56:10 +0200

    Add hyperliquid satellite

--- src/config.rs
+++ src/config.rs
@@ -117,4 +117,5 @@ pub static CONFIGURATION: Map<&'static str, (&'static str, Option<&'static str>,
 	"1430243472643719209" => ("CCXT", Some("binance"), "PENGUUSDT"),
 	"1438866334602494034" => ("CoinGecko", None, "BATUSD"),
 	"1446465138923405382" => ("CoinGecko", None, "XVGUSD"),
+	"1500945744130216047" => ("CCXT", Some("hyperliquid"), "HYPEUSDC"),
 };
